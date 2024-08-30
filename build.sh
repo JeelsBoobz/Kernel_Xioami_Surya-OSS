56ad581b4885ed8151d8803b2dbcf94d49c52203
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2024-08-30 08:33:45 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ export KBUILD_BUILD_VERSION="1"
 
 if ! [ -d "${CLANG_DIR}" ]; then
   echo "Clang not found! Cloning to ${TC_DIR}..."
-  if ! git clone --depth=1 https://gitlab.com/XSans0/weebx-clang/-/tree/release/15-gr ${TC_DIR}; then
+  if ! git clone --depth=1 https://gitlab.com/XSans0/weebx-clang-15 ${TC_DIR}; then
     echo "Cloning failed! Aborting..."
     exit 1
   fi
