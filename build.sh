fdaa810f0b69aaa0d901e3fcc48b768964c09e39
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2024-08-30 08:51:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,94 +10,123 @@
 #
 # Use this script on root of kernel directory
 
-export TZ=Asia/Jakarta
-export HEADS=$(git rev-parse --short HEAD)
 SECONDS=0 # builtin bash timer
-ZIPNAME="JeelsBoobz-${HEADS}-$(date +"%Y%m%d").zip"
-sed -i "s|CONFIG_LOCALVERSION=.*|CONFIG_LOCALVERSION=\"-JeelsBoobz-${HEADS}\"|g" "arch/arm64/configs/surya_deconfig"
-TC_DIR="$HOME/toolchain/linux-x86"
-CLANG_DIR="$HOME/toolchain/linux-x86/clang-r498229b"
-GCC_64_DIR="$HOME/toolchain/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/toolchain/arm-linux-androideabi-4.9"
-AK3_DIR="$HOME/android/AnyKernel3"
-DEFCONFIG="sm7150.config"
+LOCAL_DIR=$HOME
+ZIPNAME="JeelsBoobzKernel-Surya-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+ZIPNAME_KSU="JeelsBoobzKernel-Surya-KSU-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+TC_DIR="${LOCAL_DIR}toolchain"
+CLANG_DIR="${TC_DIR}/clang-rastamod"
+GCC_64_DIR="${LOCAL_DIR}toolchain/aarch64-linux-android-4.9"
+GCC_32_DIR="${LOCAL_DIR}toolchain/arm-linux-androideabi-4.9"
+AK3_DIR="${LOCAL_DIR}AnyKernel3"
+DEFCONFIG="surya_defconfig"
 
 export PATH="$CLANG_DIR/bin:$PATH"
-export KBUILD_BUILD_USER="JeelsBoobz"
+export KBUILD_BUILD_USER="BangJeels"
 export KBUILD_BUILD_HOST="JeelsBoobz"
+export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
 export KBUILD_BUILD_VERSION="1"
+export LOCALVERSION
 
 if ! [ -d "${CLANG_DIR}" ]; then
-  echo "Clang not found! Cloning to ${TC_DIR}..."
-  if ! git clone --depth=1 -b clang-20.0 https://gitlab.com/kutemeikito/rastamod69-clang ${CLANG_DIR}; then
-    echo "Cloning failed! Aborting..."
-    exit 1
-  fi
+echo "Clang not found! Cloning to ${TC_DIR}..."
+if ! git clone --depth=1 -b clang-20.0 https://gitlab.com/kutemeikito/rastamod69-clang ${CLANG_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
 if ! [ -d "${GCC_64_DIR}" ]; then
-  echo "gcc not found! Cloning to ${GCC_64_DIR}..."
-  if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
-    echo "Cloning failed! Aborting..."
-    exit 1
-  fi
+echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
 if ! [ -d "${GCC_32_DIR}" ]; then
-  echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
-  if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
-    echo "Cloning failed! Aborting..."
-    exit 1
-  fi
+echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
-# Setup and apply patch KernelSU in root dir
-if [ "${KSU}" = "true" ]; then
-  if ! [ -d "$KERNEL_DIR"/KernelSU ]; then
-    curl -LSs "https://raw.githubusercontent.com/kutemeikito/KernelSU/main/kernel/setup.sh" | bash -s main
-  else
-    echo -e "Setup KernelSU failed, stopped build now..."
-    exit 1
-  fi
+if [[ $1 = "-k" || $1 = "--ksu" ]]; then
+	echo -e "\nCleanup KernelSU first on local build\n"
+	rm -rf KernelSU drivers/kernelsu
+else
+	echo -e "\nSet No KernelSU Install, just skip\n"
 fi
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-  make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-  cp out/defconfig arch/arm64/configs/$DEFCONFIG
-  exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-  rm -rf out
+# Set function for override kernel name and variants
+curl -kLSs "https://raw.githubusercontent.com/kutemeikito/KernelSU/main/kernel/setup.sh" | bash -s main
+if [[ $1 = "-k" || $1 = "--ksu" ]]; then
+    echo -e "\nKSU Support, let's Make it On\n"
+    sed -i "s|CONFIG_LOCALVERSION=.*|CONFIG_LOCALVERSION=\"-JeelsBoobz-KSU\"|g" arch/arm64/configs/vendor/$DEFCONFIG
+else
+    echo -e "\nKSU not Support, let's Make it off\n"
+    sed -i 's/CONFIG_KSU=y/CONFIG_KSU=n/g' arch/arm64/configs/vendor/$DEFCONFIG
+    sed -i "s|CONFIG_LOCALVERSION=.*|CONFIG_LOCALVERSION=\"-JeelsBoobz\"|g" arch/arm64/configs/vendor/$DEFCONFIG
 fi
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
+make -j$(nproc --all) O=out \
+					  ARCH=arm64 \
+					  CC=clang \
+					  LD=ld.lld \
+					  AR=llvm-ar \
+					  AS=llvm-as \
+					  NM=llvm-nm \
+					  OBJCOPY=llvm-objcopy \
+					  OBJDUMP=llvm-objdump \
+					  STRIP=llvm-strip \
+					  CROSS_COMPILE=aarch64-linux-android- \
+					  CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+					  CLANG_TRIPLE=aarch64-linux-gnu- \
+					  Image.gz-dtb \
+					  dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-  echo -e "\nKernel compiled succesfully! Zipping up...\n"
-  if [ -d "$AK3_DIR" ]; then
-    cp -r $AK3_DIR AnyKernel3
-  elif ! git clone -q https://github.com/kutemeikito/AnyKernel3; then
-    echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-    exit 1
-  fi
-  cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-  cp out/arch/arm64/boot/dtbo.img AnyKernel3
-  rm -f *zip
-  cd AnyKernel3
-  git checkout master &> /dev/null
-  sed -i "s|kernel.string=.*|kernel.string=JeelsBoobz Kernel|g" ./anykernel.sh
-  zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-  cd ..
-  rm -rf AnyKernel3
-  rm -rf out/arch/arm64/boot
-  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-  echo "Zip: $ZIPNAME"
-else
-  echo -e "\nCompilation failed!"
-  exit 1
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+git restore arch/arm64/configs/vendor/$DEFCONFIG
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/kutemeikito/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
 fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -f *zip
+cd AnyKernel3
+git checkout main &> /dev/null
+if [[ $1 = "-k" || $1 = "--ksu" ]]; then
+zip -r9 "../$ZIPNAME_KSU" * -x '*.git*' README.md *placeholder
+else
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+fi
+cd ..
+rm -rf AnyKernel3
+rm -rf out/arch/arm64/boot
+echo -e "======================================="
+echo -e "░█─▄▀ █▀▀ █▀▀█ █▀▀▄ █▀▀ █── "
+echo -e "░█▀▄─ █▀▀ █▄▄▀ █──█ █▀▀ █── "
+echo -e "░█─░█ ▀▀▀ ▀─▀▀ ▀──▀ ▀▀▀ ▀▀▀ "
+echo -e "======================================="
+echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if [[ $1 = "-k" || $1 = "--ksu" ]]; then
+echo "Zip: $ZIPNAME_KSU"
+else
+echo "Zip: $ZIPNAME"
+fi
+else
+echo -e "\nCompilation failed!"
+exit 1
+fi
+echo "Move Zip into Home Directory"
+mv *.zip ${LOCAL_DIR}
+echo -e "======================================="
